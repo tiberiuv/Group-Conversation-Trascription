b07573ab947d99a3297c1fd1a01ab8a989e8de77
Tiberiu Simion Voicu <tiberiusimionvoicu@gmail.com>
2018-03-29 10:38:06 +0300

    minor changes in vad

--- asr_projectTests/asr_projectTests.swift
+++ asr_projectTests/asr_projectTests.swift
@@ -11,14 +11,13 @@ import AIToolbox
 
 @testable import asr_project
 class asr_projectTests: XCTestCase {
-    var file: Audio_file!
-    var file_chad_train: Audio_file!
-    var file_joey_train: Audio_file!
-    var file_monica_train: Audio_file!
-    var file_phoebe_train: Audio_file!
-    var file_rachel_train: Audio_file!
-    var file_ross_train: Audio_file!
-    var file_ross_test: Audio_file!
+    let file_chad_train: Audio_file = Audio_file(url: Bundle.main.url(forResource: "chad_joined", withExtension: "wav", subdirectory: "data_friends")!)
+    let file_joey_train = Audio_file(url: Bundle.main.url(forResource: "joey_joined", withExtension: "wav", subdirectory: "data_friends")!)
+    let file_monica_train = Audio_file(url: Bundle.main.url(forResource: "monica_joined", withExtension: "wav", subdirectory: "data_friends")!)
+    let file_phoebe_train = Audio_file(url: Bundle.main.url(forResource: "phoebe_joined", withExtension: "wav", subdirectory: "data_friends")!)
+    let file_rachel_train = Audio_file(url: Bundle.main.url(forResource: "rachel_joined", withExtension: "wav", subdirectory: "data_friends")!)
+    let file_ross_train = Audio_file(url: Bundle.main.url(forResource: "ross_joined", withExtension: "wav", subdirectory: "data_friends")!)
+    let file_ross_test = Audio_file(url: Bundle.main.url(forResource: "ross_test", withExtension: "wav", subdirectory: "data_friends")!)
     
     var chad_train: [Float]!
     var joey_train: [Float]!
@@ -39,13 +38,8 @@ class asr_projectTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 //        file = Audio_file(url :Bundle.main.url(forResource: "7127-75946-0002", withExtension: "flac")!)
         
-        file_chad_train = Audio_file(url: Bundle.main.url(forResource: "chad_joined", withExtension: "wav", subdirectory: "data_friends")!)
-        file_joey_train = Audio_file(url: Bundle.main.url(forResource: "joey_joined", withExtension: "wav", subdirectory: "data_friends")!)
-        file_monica_train = Audio_file(url: Bundle.main.url(forResource: "monica_joined", withExtension: "wav", subdirectory: "data_friends")!)
-        file_phoebe_train = Audio_file(url: Bundle.main.url(forResource: "phoebe_joined", withExtension: "wav", subdirectory: "data_friends")!)
-        file_rachel_train = Audio_file(url: Bundle.main.url(forResource: "rachel_joined", withExtension: "wav", subdirectory: "data_friends")!)
-        file_ross_train = Audio_file(url: Bundle.main.url(forResource: "ross_joined", withExtension: "wav", subdirectory: "data_friends")!)
-        file_ross_test = Audio_file(url: Bundle.main.url(forResource: "ross_test", withExtension: "wav", subdirectory: "data_friends")!)
+      
+       
         
         print(file_chad_train.format.description);
         
@@ -84,6 +78,7 @@ class asr_projectTests: XCTestCase {
         super.tearDown()
     }
     func test_fft() {
+        let file = file_chad_train;
         let buffer = AVAudioPCMBuffer(pcmFormat: file.format, frameCapacity: AVAudioFrameCount(file.file.length))
         do {
             try file.file.read(into: buffer!, frameCount: 512);
@@ -99,6 +94,7 @@ class asr_projectTests: XCTestCase {
         
     }
     func test_floatToBuffer() throws {
+        let file = file_chad_train
         let buffer = AVAudioPCMBuffer(pcmFormat: file.format, frameCapacity: 512)
         do {
             try file.file.read(into: buffer!, frameCount: 512);
@@ -113,7 +109,8 @@ class asr_projectTests: XCTestCase {
         } catch {}
         
     }
-    func test_vad(file: Audio_file ) -> [[Double]] {
+    func test_vad() {
+        let file = file_chad_train;
         let buffer = AVAudioPCMBuffer(pcmFormat: file.format, frameCapacity: AVAudioFrameCount(file.file.length))
         do {
             try file.file.read(into: buffer!)
@@ -128,15 +125,15 @@ class asr_projectTests: XCTestCase {
                 let index1 = Int(times[0] * file.format.sampleRate / 10);
                 let index2 = Int(times[1] * file.format.sampleRate / 10);
                 let s_samples = [Float](samples[index1..<index2])
-                let s_pointer = UnsafeMutablePointer(mutating: s_samples)
+//                let s_pointer = UnsafeMutablePointer(mutating: s_samples)
                 mfccs.append(contentsOf: extract_mfccs(s_samples));
-//                print("Index 1: \(index1)");
-//                print("Index 2: \(index2)");
+                print("Index 1: \(index1)");
+                print("Index 2: \(index2)");
                 times[0] = times.popLast()!;
             }
 //            mfcc.get_mfccs(pointer, and_size: Int32(speech_samples.count - 1))
         });
-        return mfccs
+        //return mfccs
     }
     func extract_mfccs(_ samples: [Float]) -> [[Double]]{
         let pointer = UnsafeMutablePointer<Float>(mutating: samples)
@@ -165,26 +162,12 @@ class asr_projectTests: XCTestCase {
         for i in 1...Int(mfcc![0]) {
             
             features.append(mfcc![i])
-            if(i % 36 == 0) {
+            if(i % 12 == 0) {
                 mfcc_array.append(features)
                 features.removeAll()
             }
         }
     }
-//    func test_mfcc_extraction(){
-//        let buffer = AVAudioPCMBuffer(pcmFormat: file.format, frameCapacity: UInt32(file.file.length))
-//        do {
-//            try file.file.read(into: buffer!)
-//        } catch {print(error)}
-//        let samples = Process_helper.buffer_to_float(buffer: buffer!);
-//        let pointer = UnsafeMutablePointer<Float>(mutating: billy_train)
-//
-//
-//        let mfcc_computer = mfcc_wrapper();
-//
-//        let x = mfcc_computer.get_mfccs(pointer, and_size: UInt32(billy_train.count))
-//
-//    }
 //    func apply_vad(buffer: AVAudioPCMBuffer) {
 //        let vad = VAD(buffer: buffer);
 //    
@@ -193,8 +176,8 @@ class asr_projectTests: XCTestCase {
     func test_classifierknn() {
         var training_samples = [knn_curve_label_pair]();
         let knn = KNNDTW()
-//        knn.configure(neighbors: 5, max_warp: 3)
-        let numFeatureSets =  15;
+        //knn.configure(neighbors: 10, max_warp: 3)
+        let numFeatureSets =  100;
         let mfcc_chad = extract_mfccs(chad_train)[0..<numFeatureSets]
         let mfcc_joey = extract_mfccs(joey_train)[0..<numFeatureSets]
         let mfcc_monica = extract_mfccs(monica_train)[0..<numFeatureSets]
@@ -206,19 +189,19 @@ class asr_projectTests: XCTestCase {
         var data_set = [[Double]](mfcc_chad + mfcc_joey + mfcc_monica + mfcc_phoebe + mfcc_rachel + mfcc_ross)
         data_set = normaliseData(data: data_set);
         for i in 0..<data_set.count {
-            if(i < 50) {
+            if(i < 100) {
                 training_samples.append(knn_curve_label_pair(curve: data_set[i].map {Float($0) }, label: "chad"))
             }
-            else if(i > 50 && i < 100) {
+            else if(i > 100 && i < 200) {
                 training_samples.append(knn_curve_label_pair(curve: data_set[i].map {Float($0) }, label: "joey"))
             }
-            else if(i > 100 && i < 150) {
+            else if(i > 200 && i < 300) {
                 training_samples.append(knn_curve_label_pair(curve: data_set[i].map {Float($0) }, label: "monica"))
             }
-            else if(i > 150 && i < 200) {
+            else if(i > 300 && i < 400) {
                 training_samples.append(knn_curve_label_pair(curve: data_set[i].map {Float($0) }, label: "phoebe"))
             }
-            else if(i > 200 && i < 250) {
+            else if(i > 400 && i < 500) {
                 training_samples.append(knn_curve_label_pair(curve: data_set[i].map {Float($0) }, label: "rachel"))
             }
             else  {
@@ -234,8 +217,8 @@ class asr_projectTests: XCTestCase {
             for mfcc in test_instance {
                 normalised_instance.append((mfcc - mean) / std_dev);
             }
-//            let prediction: knn_certainty_label_pair = knn.predict(curve_to_test: test_instance.map {Float($0) })
             let prediction: knn_certainty_label_pair = knn.predict(curve_to_test: normalised_instance.map {Float($0) })
+//            let prediction: knn_certainty_label_pair = knn.predict(curve_to_test: test_instance.map {Float($0) })
             if(prediction.label == "ross") {
                 accuracy += 1;
             }
@@ -246,10 +229,10 @@ class asr_projectTests: XCTestCase {
 
     }
     func test_ClassifierSVM(){
-//        let mfcc_chad = extract_mfccs(chad_train);
-//        let mfcc_joey = extract_mfccs(joey_train);
+        let mfcc_chad = extract_mfccs(chad_train);
+        let mfcc_joey = extract_mfccs(joey_train);
         let mfcc_monica = extract_mfccs(monica_train);
-//        let mfcc_phoebe = extract_mfccs(phoebe_train);
+        let mfcc_phoebe = extract_mfccs(phoebe_train);
         let mfcc_rachel = extract_mfccs(rachel_train);
         let mfcc_ross = extract_mfccs(ross_train);
         let mfcc_ross_test = extract_mfccs(ross_test);
@@ -260,18 +243,21 @@ class asr_projectTests: XCTestCase {
         let svm_kernel = KernelParameters(type: .radialBasisFunction, degree: 0 , gamma: 0.1, coef0: 64);
         let svm_ross = SVMModel(problemType: .c_SVM_Classification, kernelSettings: svm_kernel)
 //        let svm_imposter = SVMModel(problemType: .c_SVM_Classification, kernelSettings: svm_kernel)
-        let numFeatureSets =  22;
+        let numFeatureSets =  800;
+        var normalised_train_data: DataSet = DataSet(dataType: .classification, inputDimension: 36, outputDimension: 1);
         do {
             for i in 0..<numFeatureSets {
 //                try train_data.addDataPoint(input: vad_chad[i], dataClass: 0)
-//                try train_data.addDataPoint(input: vad_ross[i], dataClass: 1)
-//                try train_data.addDataPoint(input: mfcc_chad[i], dataClass: 0)
-//                try train_data.addDataPoint(input: mfcc_joey[i], dataClass: 0)
-//                try train_data.addDataPoint(input: mfcc_phoebe[i], dataClass: 0)
+//                try train_data.addDataPoint(input: vad_ross[i], dataClass: 0)
+                try train_data.addDataPoint(input: mfcc_chad[i], dataClass: 0)
+                try train_data.addDataPoint(input: mfcc_joey[i], dataClass: 0)
+                try train_data.addDataPoint(input: mfcc_phoebe[i], dataClass: 0)
                 try train_data.addDataPoint(input: mfcc_monica[i], dataClass: 0)
-                try train_data.addDataPoint(input: mfcc_rachel[i], dataClass: 0)
-                try train_data.addDataPoint(input: mfcc_ross[i], dataClass: 1)
+                
+                try train_data.addDataPoint(input: mfcc_rachel[i], dataClass: 1)
+                try train_data.addDataPoint(input: mfcc_ross[i], dataClass: 0)
             }
+            normalised_train_data = normaliseData_mlpack(train: train_data);
 //            for i in 0..<mfcc_ross_test.count {
 //                try test_data.addTestDataPoint(input: mfcc_ross_test[i])
 //                //try test_data.addDataPoint(input: mfcc_ross_test[i], dataClass: 1)
@@ -279,9 +265,10 @@ class asr_projectTests: XCTestCase {
         } catch {print("Error creating testing and training data : \(error)")}
 
         do {
-            try svm_ross.trainClassifier(train_data)
+//            try svm_ross.trainClassifier(train_data)
+            try svm_ross.trainClassifier(normalised_train_data)
             
-//            try svm_imposter.trainClassifier(test_data);
+
         } catch {print("Error in training classifier : \(error)")}
 //        do {
 //            try svm_ross.classify(test_data);
@@ -290,9 +277,14 @@ class asr_projectTests: XCTestCase {
             var accuracy = 0.0;
             var test_mfcc = mfcc_ross_test
             for i in 0..<test_mfcc.count {
-                let prediction = try svm_ross.classifyOne(test_mfcc[i]);
+                var normal_vector = test_mfcc[i];
+                for k in 0..<test_mfcc[i].count {
+                    normal_vector[k] = (normal_vector[k] - mean) / std_dev;
+                }
+//                let prediction = try svm_ross.classifyOne(test_mfcc[i]);
+                let prediction = try svm_ross.classifyOne(normal_vector);
                 print("Prediction \(prediction)");
-                if( prediction == 1) {
+                if( prediction == 0) {
                     accuracy += 1;
                 }
             }
@@ -314,7 +306,43 @@ class asr_projectTests: XCTestCase {
 
         } catch {print("Error getting the result of classification : \(error)")}
 
-
+    }
+    func normaliseData_mlpack(train: DataSet) -> DataSet {
+        let norm_data: DataSet = DataSet(dataType: train.dataType, inputDimension: train.inputDimension, outputDimension: train.outputDimension);
+        var mean: Double = 0.0;
+        var std_dev: Double = 0.0;
+        
+        do {
+            var num_features = try train.getInput(0).count;
+            for i in 0..<train.size {
+                let input_vector = try train.getInput(i);
+                for k in 0..<input_vector.count {
+                    mean += input_vector[k];
+                }
+            }
+            mean /= Double(train.size * num_features)
+            for i in 0..<train.size {
+                let input_vector = try train.getInput(i);
+                for k in 0..<input_vector.count  {
+                    std_dev += pow(input_vector[k] - mean, 2);
+                }
+            }
+            std_dev /= Double(train.size * num_features)
+            
+            for i in 0..<train.size {
+                var input_vector = try train.getInput(i);
+                let out_class = try train.getClass(i);
+                for k in 0..<input_vector.count {
+                    input_vector[k] = (input_vector[k] - mean ) / std_dev;
+                    
+                }
+                try norm_data.addDataPoint(input: input_vector, dataClass: out_class)
+            }
+            self.mean = mean;
+            self.std_dev = std_dev;
+        } catch { print("error \(error)")}
+        
+        return norm_data
     }
     func normaliseData(data: [[Double]]) -> [[Double]] {
         var norm_data = [[Double]](data)
